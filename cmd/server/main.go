2790938a5e8c34d370d3c18ac8d9b3027f9573bf
radugin <radugin-a@bk.ru>
2025-11-26 16:00:25 +0300

    add const

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -7,6 +7,10 @@ import (
 	"net/http"
 )
 
+const (
+	httpPort = ":3000"
+)
+
 func main() {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
@@ -18,7 +22,7 @@ func main() {
 		}
 	})
 
-	if err := http.ListenAndServe(":3000", r); err != nil {
+	if err := http.ListenAndServe(httpPort, r); err != nil {
 		panic(err)
 	}
 }
