e33a0e26fef1627a6e3bf20018b7e1a13f82fa85
radugin <radugin-a@bk.ru>
2025-11-28 14:14:15 +0300

    add http client

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"github.com/ArtemRadugin/weather-service/internal/client/http/geocoding"
+	"github.com/ArtemRadugin/weather-service/internal/client/http/open_meteo"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/go-co-op/gocron/v2"
@@ -19,11 +22,34 @@ func main() {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 
+	httpClient := &http.Client{
+		Timeout: 10 * time.Second,
+	}
+	geocodingClient := geocoding.NewClient(httpClient)
+	openMeteoClient := open_meteo.NewClient(httpClient)
+
 	r.Get("/{city}", func(w http.ResponseWriter, r *http.Request) {
 		city := chi.URLParam(r, "city")
 		fmt.Printf("received request for city: %s\n", city)
 
-		_, err := w.Write([]byte("welcome"))
+		geoResp, err := geocodingClient.GetCoordinates(city)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		opMetResp, err := openMeteoClient.GetTemperature(geoResp.Latitude, geoResp.Longitude)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		row, err := json.Marshal(opMetResp)
+		if err != nil {
+			log.Println(err)
+		}
+
+		_, err = w.Write(row)
 		if err != nil {
 			log.Println(err)
 		}
